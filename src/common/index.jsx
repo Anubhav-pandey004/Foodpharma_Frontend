dbaa3fc1487c0964fc0993efef6da6855b31a8ba
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-13 18:58:53 +0530

    Update index.jsx

--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,6 +1,6 @@
 
 
-const backendDomain = import.meta.env.VITE_BACKEND_URL || "https://food-pharma-backend.netlify.app"
+const backendDomain = import.meta.env.VITE_BACKEND_URL || "https://food-pharma-backend.onrender.com"
 // const backendDomain = "https://foodpharma-backend.onrender.com"
 
 const SummaryApi ={
